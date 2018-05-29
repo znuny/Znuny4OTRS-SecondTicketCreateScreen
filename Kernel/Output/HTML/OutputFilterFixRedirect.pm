873dc06bdb74e94b62018e50b8bfd58a5ceea52c
Jens Pfeifer <jens.pfeifer@znuny.com>
2018-05-29 14:43:18 +0000

    Issue #3, Redirect generates wrong links.

--- Kernel/Output/HTML/OutputFilterFixRedirect.pm
+++ Kernel/Output/HTML/OutputFilterFixRedirect.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -55,7 +55,7 @@ sub Run {
         && $LayoutObject->{UserCreateNextMask} =~ m{\AAgent(Ticket)?Zoom};
 
     # rewrite redirect
-    ${ $Param{Data} } =~ s{Action=(AgentTicket(Phone|Email))}{Action=$LayoutObject->{Action}};
+    ${ $Param{Data} } =~ s{Action=(AgentTicket(Phone|Email))\b}{Action=$1Second};
 
     return;
 }
