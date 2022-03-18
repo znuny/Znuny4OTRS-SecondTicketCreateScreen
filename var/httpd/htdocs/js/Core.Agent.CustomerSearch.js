929e9d4d519afaec0c9b5ee60ea2e9cfe6abd8ae
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2022-03-18 13:54:19 +0100

    Applied CP.

--- var/httpd/htdocs/js/Core.Agent.CustomerSearch.js
+++ var/httpd/htdocs/js/Core.Agent.CustomerSearch.js
@@ -1,5 +1,5 @@
 // --
-// Copyright (C) 2001-2022 OTRS AG, https://otrs.com/
+// Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 // Copyright (C) 2012-2022 Znuny GmbH, http://znuny.com/
 // --
 // $origin: otrs - 5ca27d62fd6e9fb6e8019cd22fbe199934c7c958 - var/httpd/htdocs/js/Core.Agent.CustomerSearch.js
